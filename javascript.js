9d6461fc6a7356f6a00e31fc8ad2c74bada13aa4
Karan Agarwal (KKA) <karanyobro@gmail.com>
2022-11-28 23:47:37 +0530

    update update

--- javascript.js
+++ javascript.js
@@ -9,3 +9,5 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+
--- submit.php
+++ submit.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/x-icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Q_magazine_logo.svg/2034px-Q_magazine_logo.svg.png">
+	<title>Document</title>
+</head>
+<body>
+<div class="buttonww">
+        <button class="glow-on-hover" type="button" onclick="window.location.href='index.html';">Go back to home Page</button>
+      </div>
+</br>
+</body>
+</html>
+
+
 <?php
 include('form.php');
 $msg="";
