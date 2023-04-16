36ee2fd635bb3463e868a5d1c0e73b428fbd2b83
Harshvardhan Singh Jadon <jadonharsh109@gmail.com>
2023-04-16 23:21:12 +0530

    docker-compose initiated

--- form.php
+++ form.php
@@ -1,7 +1,7 @@
 <?php
-//$con=mysqli_connect('localhost','root','','epiz_32211115_quiska');
+$con = mysqli_connect('kkadb', 'root', 'karan@123', 'epiz_32211115_quiska');
 ?>
 
 <?php
-$con=mysqli_connect('sql.freedb.tech','freedb_KKA123','a3R%V#TsSAP36W#','freedb_quiska');
+// $con=mysqli_connect('sql.freedb.tech','freedb_KKA123','a3R%V#TsSAP36W#','freedb_quiska');
 ?>
