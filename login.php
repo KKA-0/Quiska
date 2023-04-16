563d5039819f41b3420e38d65ed5880ff8ecd3f2
Harshvardhan Singh Jadon <jadonharsh109@gmail.com>
2023-04-17 01:03:02 +0530

    docker-compose initiated

--- login.php
+++ login.php
@@ -1,4 +1,109 @@
 
+<?php
+require_once("form.php");
+
+$username = $password = $email = $confirm_password = "";
+$username_err = $password_err = $email_err = $confirm_password_err = "";
+
+if ($_SERVER['REQUEST_METHOD'] == "POST"){
+
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Username cannot be blank";
+    }
+    else{
+        $sql = "SELECT id FROM users WHERE username = ?";
+        $stmt = mysqli_prepare($con, $sql);
+        if($stmt)
+        {
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+            // Set the value of param username
+            $param_username = trim($_POST['username']);
+
+            // Try to execute this statement
+            if(mysqli_stmt_execute($stmt)){
+                mysqli_stmt_store_result($stmt);
+                if(mysqli_stmt_num_rows($stmt) == 1)
+                {
+                    $username_err = "This username is already taken"; 
+                }
+                else{
+                    $username = trim($_POST['username']);
+                }
+            }
+            else{
+                echo "Something went wrong";
+            }
+        }
+    }
+
+    mysqli_stmt_close($stmt);
+
+
+// email
+if(empty(trim($_POST['email']))){
+  $email_err = "email cannot be blank";
+}
+elseif(strlen(trim($_POST['email'])) < 5){
+  $email_err = "email cannot be less than 5 characters";
+}
+else{
+  $email = trim($_POST['email']);
+}
+// Check for password
+if(empty(trim($_POST['password']))){
+    $password_err = "Password cannot be blank";
+}
+elseif(strlen(trim($_POST['password'])) < 5){
+    $password_err = "Password cannot be less than 5 characters";
+}
+else{
+    $password = trim($_POST['password']);
+}
+
+// Check for confirm password field
+if(trim($_POST['password']) !=  trim($_POST['confirm_password'])){
+    $password_err = "Passwords should match";
+}
+
+
+// If there were no errors, go ahead and insert into the database
+if(empty($username_err) && empty($email_err) && empty($password_err) && empty($confirm_password_err))
+{
+    $sql = "INSERT INTO users (username, email,  password) VALUES (?, ?, ?)";
+    $stmt = mysqli_prepare($con, $sql);
+    if ($stmt)
+    {
+        mysqli_stmt_bind_param($stmt, "sss", $param_username, $param_email, $param_password);
+
+        // Set these parameters
+        $param_username = $username;
+        $param_email = $email;
+        $param_password = password_hash($password, PASSWORD_DEFAULT);
+
+        // Try to execute the query
+        if (mysqli_stmt_execute($stmt))
+        {
+            header("location: index.html");
+        }
+        else{
+            echo "Something went wrong... cannot redirect!";
+        }
+    }
+    mysqli_stmt_close($stmt);
+}
+mysqli_close($con);
+}
+
+?>
+
+<?php
+//login verification
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
