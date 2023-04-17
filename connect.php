4dfb9aa10c3ddfa8c7be22a474fc28d432b5e920
Harshvardhan Singh Jadon <jadonharsh109@gmail.com>
2023-04-17 12:31:26 +0530

    Connectivity Error

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@ $user = 'root';
 
 $pass = 'karan@123';
 
-$conn = mysqli_connect($host, $user, $pass);
+$conn = mysql_connect($host, $user, $pass);
 if (!$conn) {
     die("Connection failed: ");
 }echo "Connected successfully";
