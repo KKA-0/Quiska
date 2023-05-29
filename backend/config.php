caf0fd0b067d8850b0c7d2d1f51bc67849b15d4f
KKA-0 <karanyobro@gmail.com>
2023-05-29 16:45:32 +0530

    re-designing

---
+++ backend/config.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+$dbHost = 'localhost';
+$dbName = 'quiska';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbc=mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+
+?>
--- form.php
+++
@@ -1,7 +0,0 @@
-<?php
-//$con=mysqli_connect('localhost','root','','epiz_32211115_quiska');
-?>
-
-<?php
-$con=mysqli_connect('sql105.epizy.com','epiz_33250279','WD38MJch2PWoiQ','epiz_33250279_quiska');
-?>
